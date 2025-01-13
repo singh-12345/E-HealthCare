01188e990dba40b99de10625a9cdd8ed9598d9b3
Aman Kumar Singh <110766713+singh-12345@users.noreply.github.com>
2025-01-14 02:07:40 +0530

    Update apis.js

--- client/src/services/apis.js
+++ client/src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://e-healthcare-server-sczm.onrender.com"
+const BASE_URL = "https://e-healthcare-server-sczm.onrender.com/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
